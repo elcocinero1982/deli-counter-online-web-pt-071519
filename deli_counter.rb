6b6002c8d84f3ef63d6c4005590173e428777e4a
jose luis <cortezluisjose@gmail.com>
2019-07-24 05:58:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,27 +3,3 @@
     puts "The line is currently empty."
   
  end
-  def line((other_deli)) 
-  
-
-end
- def take_a_number
-   
- end
- 
-def now_serving
-  puts "There is nobody waiting to be served!"
-  
-  
-end
-
-
-
-  
-  
-
-
-def now_serving(katz_deli)
-  now_serving = other_deli
-  
-end
