91519f563fb254aed74cfbed29328d2049dec967
jose luis <cortezluisjose@gmail.com>
2019-07-24 05:43:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@
   
  end
   def line((other_deli)) 
-  puts ("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
+  
 
 end
  def take_a_number
@@ -14,5 +14,13 @@ end
 def now_serving
   
   
+  
+end
+def now_serving(katz_deli)
+  
+  
+end
+
+def now_serving(other_deli)
   
 end
